8ee2b7fb2201be1cc73661777447740769cbe305
Lincoln Eulogio <69059639+eulogiohuanca@users.noreply.github.com>
2022-01-20 11:10:44 -0500

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+(function(){
+    const slider = [...document.querySelectorAll('.slider__body')];
+    const arrowNext = document.querySelector('#next');
+    const arrowBefore = document.querySelector('#before');
+    let value;
+
+    arrowNext.addEventListener('click', ()=>changePosition(1));
+    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+
+    function changePosition(change) {
+        const currentElement = Number(document.querySelector('.slider__body---show').dataset.id);
+
+        value = currentElement;
+        value += change;
+
+        if(value === 0 || value == slider.length+1) {
+            value = value === 0 ? slider.length : 1;
+        }
+
+        slider[currentElement-1].classList.toggle('slider__body---show');
+        slider[value-1].classList.toggle('slider__body---show');
+    }
+})()
